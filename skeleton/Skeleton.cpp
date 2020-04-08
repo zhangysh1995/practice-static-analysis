c19c6acc81cb63cfa7ac61d9d32dd42b9415b38d
Yushan ZHANG <yzhanghw@cse.ust.hk>
2020-04-08 15:16:01 +0800

    fix for llvm10

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,17 +1,22 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
 using namespace llvm;
 
 namespace {
-  struct SkeletonPass : public FunctionPass {
+  struct SkeletonPass : public ModulePass {
     static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
+    SkeletonPass() : ModulePass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
+     bool runOnModule(Module &M) override{
+        for (auto &it : M) {
+            errs() << "My name is: " << it.getName() << "\n";
+        }
+      errs() << "I saw a function called " << M.getName() << "!\n";
       return false;
     }
   };
@@ -19,12 +24,12 @@ namespace {
 
 char SkeletonPass::ID = 0;
 
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new SkeletonPass());
-}
-static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
+static RegisterPass<SkeletonPass> X("skeleton", "Hello World Pass",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
+
+static llvm::RegisterStandardPasses Y(
+        llvm::PassManagerBuilder::EP_EarlyAsPossible,
+        [](const llvm::PassManagerBuilder &Builder,
+           llvm::legacy::PassManagerBase &PM) { PM.add(new SkeletonPass()); });
+
